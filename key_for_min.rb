a2d8c07e2a00535e9a0769e7e6b2235da5e45c69
Jed Peek <jedpeek@yahoo.com>
2018-06-06 02:53:39 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,10 +9,8 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if min_value == nil
         min_value = value
-        puts min_value
       elsif min_value >= value
         min_value = value
-        puts min_value
       end
     end
   return min_value
