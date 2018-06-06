de8e8e89cac74985c81e17c808caf2012b8c44b5
Jed Peek <jedpeek@yahoo.com>
2018-06-06 02:57:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,10 +13,10 @@ def key_for_min_value(name_hash)
         key_value[0] = key
       elsif min_value >= value
         min_value = value
-        key_value[0]
+        key_value[0] = key
       end
     end
-  return min_value
+  return key_value[0]
 end
 hash = {washington: 1, lincoln: 5, franklin: 100}
 key_for_min_value(hash)
